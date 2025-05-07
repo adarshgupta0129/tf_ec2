110f40442995a46dd11a860dce4e1adb0d2737e5
adarshgupta0129 <adarshgupta0129@gmail.com>
2025-05-07 14:22:53 +0530

    changes main tf file new3

--- main.tf
+++ main.tf
@@ -2,27 +2,42 @@ provider "aws" {
   region = var.aws_region
 }
 
-data "aws_vpc" "default" {
-  default = true
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 }
 
-data "aws_subnet" "default" {
-  filter {
-    name   = "default-for-az"
-    values = ["true"]
+resource "aws_subnet" "main" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_security_group" "main" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_instance" "free_tier_ec2" {
-  ami           = var.ami_id
-  instance_type = "t2.micro"
-  key_name      = var.key_name
-  subnet_id     = data.aws_subnet.default.id
+  ami                    = var.ami_id
+  instance_type          = "t2.micro"
+  key_name               = var.key_name
+  subnet_id              = aws_subnet.main.id
+  vpc_security_group_ids = [aws_security_group.main.id]
 
   tags = {
     Name = "FreeTierEC2"
