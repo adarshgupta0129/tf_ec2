79768c7136879a04f81727843b1adb418dc150e5
adarshgupta0129 <adarshgupta0129@gmail.com>
2025-05-07 12:22:47 +0530

    updated key name

--- variables.tf
+++ variables.tf
@@ -1,12 +1,12 @@
 variable "aws_region" {
-  default = "us-east-1"
+  default = "eu-north-1"
 }
 
 variable "ami_id" {
-  default = "ami-0c2b8ca1dad447f8a" # Amazon Linux 2 in us-east-1
+  default = "ami-09eb4311cbaecf89f" # Amazon Linux 2 in us-east-1
 }
 
 variable "key_name" {
-  description = "key_pair_learn_2.pem"
+  description = "key_pair_learn_2"
   type        = string
 }
