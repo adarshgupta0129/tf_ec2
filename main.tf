fa056d5ab26f1e642353d3768ba543f5ec00dc33
adarshgupta0129 <adarshgupta0129@gmail.com>
2025-05-07 14:25:51 +0530

    changes main tf file new4

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "main" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
+  availability_zone       = "eu-north-1a"
   map_public_ip_on_launch = true
 }
 
@@ -34,7 +35,7 @@ resource "aws_security_group" "main" {
 
 resource "aws_instance" "free_tier_ec2" {
   ami                    = var.ami_id
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   key_name               = var.key_name
   subnet_id              = aws_subnet.main.id
   vpc_security_group_ids = [aws_security_group.main.id]
